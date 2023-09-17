45a41091b4ddb108e05b58528bc777c807a3c895
Melk de Sousa <melk.sousap@gmail.com>
2023-09-17 14:45:37 -0300

    🐞 fix: remove url validation

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,7 +6,7 @@ const envSchema = z.object({
     OPENAI_KEY: z.string(),
     PORT: z.string().transform(value => Number(value)),
     NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string(),
     HOST: z.string().optional()
 })
 
